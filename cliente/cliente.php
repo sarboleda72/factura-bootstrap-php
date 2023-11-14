3d8de43eca4e2c306d106aa09c8e9865e1db96d6
sarboleda72 <sarboleda72@gmail.com>
2023-11-14 15:08:27 -0500

    Se finaliza el de clientes con modales

--- cliente/cliente.php
+++ cliente/cliente.php
@@ -8,10 +8,10 @@ include('./scripts/head.php');
 $query = "SELECT * FROM cliente";
 $resultado = $conexion->query($query);
 ?>
-<div class="container mt-4">
+<div class="container">
     <h3>CLIENTES</h3>
-    <table class="table">
-        <thead class="thead-dark">
+    <table class="table table-striped">
+        <thead>
             <tr>
                 <th>Nombre</th>
                 <th>Direccion</th>
@@ -19,6 +19,14 @@ $resultado = $conexion->query($query);
                 <th>Acciones</th>
             </tr>
         </thead>
+        <tfoot>
+            <tr>
+                <th>Nombre</th>
+                <th>Direccion</th>
+                <th>Telefono</th>
+                <th>Acciones</th>
+            </tr>
+        </tfoot>
         <tbody>
             <?php
             while ($fila = $resultado->fetch_assoc()) {
@@ -26,15 +34,14 @@ $resultado = $conexion->query($query);
                 echo "<td>" . $fila["nombre"] . "</td>";
                 echo "<td>" . $fila["direccion"] . "</td>";
                 echo "<td>" . $fila["telefono"] . "</td>";
-                echo "<td>".'<button class="btn btn-primary btn-sm btnEditarUsuario" data-id="' . $fila["id_cliente"]. '" data-toggle="modal" data-target="#modalActualizarUsuario'.$fila["id_cliente"].'">' .'<i class="material-icons">Editar</i>' .'</button>'.'<button class="btn btn-danger btn-sm btnBorrarUsuario" data-id="' . $fila["id_cliente"] . '" data-toggle="modal" data-target="#modalEliminarPersona'.$fila["id_cliente"].'">' .'<i class="material-icons">Eliminar</i>' .'</button>'."</td>";
+                echo "<td>" . '<button class="btn btn-primary btn-sm btnEditarUsuario" data-id="' . $fila["id_cliente"] . '" data-toggle="modal" data-target="#modalActualizarUsuario' . $fila["id_cliente"] . '">' . '<i class="material-icons">Editar</i>' . '</button>' . '<button class="btn btn-danger btn-sm btnBorrarUsuario" data-id="' . $fila["id_cliente"] . '" data-toggle="modal" data-target="#modalEliminarPersona' . $fila["id_cliente"] . '">' . '<i class="material-icons">Eliminar</i>' . '</button>' . "</td>";
                 echo "</tr>";
-                echo '<div class="modal fade" id="modalActualizarUsuario'. $fila["id_cliente"].'" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">';
+                echo '<div class="modal fade" id="modalActualizarUsuario' . $fila["id_cliente"] . '" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">';
                 include('modal_actualizar.php');
                 echo '</div>';
-                echo '<div class="modal fade" id="modalEliminarPersona'.$fila["id_cliente"].'" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">';
+                echo '<div class="modal fade" id="modalEliminarPersona' . $fila["id_cliente"] . '" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">';
                 include('modal_eliminar.php');
                 echo '</div>';
-                
             }
             ?>
         </tbody>
